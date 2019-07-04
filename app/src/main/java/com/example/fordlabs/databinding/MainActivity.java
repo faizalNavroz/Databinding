ae771371eea6deb7b3f19a1d2af599847a9a5d8d
faizal <sfaizalfarhaan@gmail.com>
2019-07-04 18:59:22 +0530

    initial commit for DataBinding

---
+++ app/src/main/java/com/example/fordlabs/databinding/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.fordlabs.databinding;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ActivityMainBinding binding
+    }
+}
